0ceafe0606b50aecb155041b65b2a485ad99e236
jaehwanjoa <90813478+jaehwanjoa@users.noreply.github.com>
2026-08-06 14:01:21 +0900

    Update lambda_function.py

--- AI_Secu_Agent/lambda_function.py
+++ AI_Secu_Agent/lambda_function.py
@@ -1,44 +1,9 @@
-from orchestrator.planner import Planner
-from orchestrator.tool_mapping import (
-    get_mapping
-)
-from mcp.athena.query_generator import (
-    AthenaQueryGenerator
-)
+def lambda_handler(event, context):
 
+    question = event.get("question")
+    customer = event.get("customer")
 
-class Orchestrator:
-
-    @classmethod
-    def process(
-        cls,
-        question
-    ):
-
-        # 1. 질문 분석
-        plan = Planner.parse(
-            question
-        )
-
-        # 2. Intent 매핑
-        mapping = get_mapping(
-            plan["intent"]
-        )
-
-        # 3. SQL 생성
-        sql = AthenaQueryGenerator.build(
-            plan
-        )
-
-        # 4. MCP 호출
-        result = cls.execute_athena(
-            sql
-        )
-
-        # 5. Bedrock 요약
-        response = cls.generate_answer(
-            question,
-            result
-        )
-
-        return response
+    return Orchestrator.process(
+        question=question,
+        customer=customer
+    )
