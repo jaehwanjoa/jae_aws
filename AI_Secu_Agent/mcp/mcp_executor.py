48fe042a6853ef02cd55cbf2c22d45055364134e
jaehwanjoa <90813478+jaehwanjoa@users.noreply.github.com>
2026-08-06 16:52:14 +0900

    Update mcp_executor.py

--- AI_Secu_Agent/mcp/mcp_executor.py
+++ AI_Secu_Agent/mcp/mcp_executor.py
@@ -1,3 +1,7 @@
+import boto3
+import time
+
+
 class MCPExecutor:
 
     @classmethod
@@ -6,11 +10,55 @@ class MCPExecutor:
         query: str
     ):
 
-        return {
-            "tool": "manage_aws_athena_query_executions",
+        athena = boto3.client(
+            "athena",
+            region_name="ap-northeast-2"
+        )
 
-            "operation":
-                "start-query-execution",
+        response = athena.start_query_execution(
+            QueryString=query,
+            QueryExecutionContext={
+                "Database": "jaehwan-aws-waf"
+            },
+            ResultConfiguration={
+                "OutputLocation":
+                    "s3://202040710-jaehwan-test/"
+            }
+        )
 
-            "query": query
-        }
+        query_id = response[
+            "QueryExecutionId"
+        ]
+
+        while True:
+
+            status = athena.get_query_execution(
+                QueryExecutionId=query_id
+            )
+
+            state = status[
+                "QueryExecution"
+            ][
+                "Status"
+            ][
+                "State"
+            ]
+
+            if state == "SUCCEEDED":
+                break
+
+            if state in [
+                "FAILED",
+                "CANCELLED"
+            ]:
+                raise Exception(
+                    f"Athena Query {state}"
+                )
+
+            time.sleep(2)
+
+        results = athena.get_query_results(
+            QueryExecutionId=query_id
+        )
+
+        return results
