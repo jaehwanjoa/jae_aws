71c3290968a15268677b96bb3a8bb98f4cb610b3
jaehwanjoa <90813478+jaehwanjoa@users.noreply.github.com>
2026-08-06 13:59:06 +0900

    Create table_catalog.py

---
+++ AI_Secu_Agent/orchestrator/table_catalog.py
@@ -0,0 +1,42 @@
+TABLE_CATALOG = {
+
+    "lotte": {
+        "database": "{athena Database}",
+        "table_name": "ons_lotte",
+        "description" : "Lotte WAF Logs"
+    },
+
+    "tving": {
+        "database": "{athena Database}",
+        "table_name": "ons_tving",
+        "description" : "tving WAF Logs"
+
+    },
+
+    "pentaech": {
+        "database": "{athena Database}",
+        "table_name": "ons_pentaech",
+        "description" : "pentaech WAF Logs"
+
+    },
+
+    "unban": {
+        "database": "{athena Database}",
+        "table_name": "ons_unban",
+        "description" : "unban WAF Logs"
+
+    },
+
+    "cjl_an2": {
+        "database": "{athena Database}",
+        "table_name": "ons_cjl_am2",
+                "description" : "Landingzon seoul WAF Logs"
+
+    },
+
+    "cjl_ue1": {
+        "database": "{athena Database}",
+        "table_name": "ons_cjl-ue1",
+        "description" : "Landingzon global WAF Logs"
+    }
+}
